b10dae0ae7a26ba5507df5c4ad614260766d490d
Femi-Benjamin <benjaminolufemi16@gmail.com>
2022-10-01 14:16:05 -0700

    v1

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,9 +12,9 @@ const Signup = () => {
           <form>
             <input className='w-[417px] h-[86px] mt-6 items-center text-center not-italic font-[400px] text-[16px] leading-[18px] border-2 bg-transparent' type="email" id='email' placeholder='enter email or number' />
 
-            <input className='w-[417px] h-[86px] mt-6 items-center text-center not-italic font-[400px] text-[16px] leading-[18px] border-2 bg-transparent mb-[28px]' type="password" id='password' placeholder='email password' />
+            <input className='w-[417px] h-[86px] mt-6 items-center text-center not-italic font-[400px] text-[16px] leading-[18px] border-2 bg-transparent mb-[30px]' type="password" id='password' placeholder='email password' />
 
-            <button className='w-[199px] h-[53px] bg-[#0f0b54] hover:bg-[#577DBC] not-italic font-[400px] text-[20px] leading-[23px] text-white rounded-md font-semibold'>SUBMIT</button>
+            <button className='w-[199px] h-[53px] bg-[#0f0b54] hover:bg-[#577DBC] not-italic font-[400px] text-[20px] leading-[23px] text-white rounded-md font-semibold mb-10'>SUBMIT</button>
 
             <div className='items-start text-left ml-[81px] mb-[16px]'>
               <input type="radio" />
